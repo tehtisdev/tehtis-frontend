0dca0cebd3e94e36df2ada280d0b80fa9bf89f0e
onni fischer <onni.fischer@gmail.com>
2025-10-31 11:10:09 +0200

    azure

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/types/importMeta.d.ts" />
-// 31.10.2025 uus commit, testataan toimiiko azure-autentikaatio
+// 31.10.2025 uus commit, toimiiko azure-autentikaatio
 interface ImportMetaEnv {
   readonly VITE_URL: string;
   readonly VITE_PRIVACY_POLICY: string;
